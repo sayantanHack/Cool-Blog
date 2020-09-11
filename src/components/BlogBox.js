8d30785e4ac3aed8dff28356563be75a19dae0c4
sayantanhack <sayantandatta97@gmail.com>
2020-09-11 22:50:03 +0530

    Added the Grid Blogbox components.

--- src/components/BlogBox.js
+++ src/components/BlogBox.js
@@ -1,18 +1,26 @@
 import React from "react";
-import {Box, makeStyles} from "@material-ui/core";
-//import useStyle from "./Appbar";
+import {Box, Container, makeStyles, Typography } from "@material-ui/core";
+import GridContent from "./GridContent";
 
 
 const useStyle = makeStyles((theme)=>({
     hero: {
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5) 
-        url('https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJ
-        hcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80')
-        )`,
-        height: "500px",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1594691564350-ebcbc4d38767?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1039&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+        height: 300,
+        fontSize: "3em"
+    }
+    
     }
 
 })
@@ -24,11 +32,20 @@ function BlogBox(){
     const classes = useStyle()
 
     return(
-        <Box className={classes.hero}>
-            <Box>
-                React Blog 
+        <div>
+            <Box className={classes.hero}>
+                <Box>
+                    React Blog 
+                </Box>
             </Box>
-    </Box>
+            <Container maxWidth="lg" className={classes.blogContainer}>
+                <Typography variant="h4" className={classes.blogTitle}>
+                    Articles
+                </Typography>
+                <GridContent />
+            </Container>
+        </div>
+        
     )
 }
 
